f06a85763af15954bb50fe8f21d9f8ae81995df1
spooality <spooality@gmail.com>
2017-06-19 13:43:31 -0500

    Pay function is working independently.

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ var totalPaymentsInput = $('input[name=total-payments]');
 var extraPaymentInput = $('input[name=extra-payment]');
 var monthsToPayoffInput = $('input[name=months-to-payoff]');
 
+// We should focus in on the descriptionInput so the user can start typing right away.
+descriptionInput.focus();
+
 // Now, we can describe the model that will be used to build the components on the page.
 // All values are in pennies, not dollars and cents.
 // If we used dollars and cents, we'd have to use floating point numbers and javascrit
@@ -23,18 +26,81 @@ var Debt = function(description, apr, balance, payment) {
     this.apr = apr;
     this.balance = balance;
     this.payment = payment;
+    this.register = [new Payment(0, 0, 0, balance)];
 
-    this.pay = function(amount, isOverflow) {
-        var interest, principle, overflow;
+    this.interest = function() {
+        return parseInt(Math.round((30 / 365) * (this.apr / 10000) * (this.balance / 100) * 100));
     }
+
+    // Amount must be given in cents, not dollars.cents.
+    this.pay = function(amount, isOverflow) {
+        var period = this.register.length;
+        var interest = isOverflow ? 0 : this.interest();
+        var principle = amount - interest;
+        var balance = this.balance;
+        var overflow = 0;
+
+        if(balance === 0) {
+            interest = 0;
+            principle = 0;
+            overflow = amount;
+        } else if(principle > balance) {
+            overflow = principle - balance;
+            principle = balance;
+            balance = 0;
+        } else {
+            balance -= principle;
+        }
+
+        this.register.push(new Payment(period, interest, principle, balance));
+        model.balance -= principle;
+        this.balance = balance;
+    }
+}
+
+// The payment object.
+var Payment = function(period, interest, principle, balance) {
+    this.period = period;
+    this.interest = interest;
+    this.principle = principle;
+    this.balance = balance;
 }
 
 // We need to add Debt objects to our model.
 function addDebt(description, apr, balance, payment) {
-    apr = parseFloat(apr);
+    apr = Math.round(parseFloat(apr * 100));
     balance = Math.round(parseInt(balance * 100));
     payment = Math.round(parseInt(payment * 100));
 
     model.balance += balance;
     model.debts.push(new Debt(description, apr, balance, payment));
 }
+
+// When the user clicks "Add Debt", this happens...
+$('#add-debt-form').submit(function(event) {
+    event.preventDefault();
+
+    function isEmpty(value) {
+        if(value === 'undefined' || value === 'null' || value === '') {
+            return true;
+        }
+
+        return false;
+    }
+
+    var error = 
+        isEmpty(descriptionInput.val()) ||
+        isEmpty(aprInput.val()) ||
+        isEmpty(balanceInput.val( )) ||
+        isEmpty(paymentInput.val()) ||
+        isNaN(aprInput.val()) ||
+        isNaN(balanceInput.val()) ||
+        isNaN(paymentInput.val());
+
+    if(!error) {
+        addDebt(descriptionInput.val(), aprInput.val(), balanceInput.val(), paymentInput.val());
+        descriptionInput.focus();
+    } else {
+        console.log("Invalid input error.");
+    }
+});
